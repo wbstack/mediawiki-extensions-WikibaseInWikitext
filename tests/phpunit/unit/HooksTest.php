d0ac236624eeb4cd9e2a2b3c0cc921f65556588b
addshore <addshorewiki@gmail.com>
2020-02-02 13:40:33 +0000

    WikibaseInWikitext extension
    Custom extension (not in a git repo)
    to add the <sparql> tag for wikitext.
    
    Request from Andra.
    
    This patch also deploys it..

---
+++ tests/phpunit/unit/HooksTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MediaWiki\Extension\WikibaseInWikitext\Tests;
+
+use MediaWiki\Extension\WikibaseInWikitext\Hooks;
+
+/**
+ * @coversDefaultClass \MediaWiki\Extension\WikibaseInWikitext\Hooks
+ */
+class HooksTest extends \MediaWikiUnitTestCase {
+
+	/**
+	 * @covers ::onBeforePageDisplay
+	 */
+	public function testOnBeforePageDisplayVandalizeIsTrue() {
+		$config = new \HashConfig( [
+			'WikibaseInWikitextVandalizeEachPage' => true
+		] );
+		$outputPageMock = $this->getMockBuilder( \OutputPage::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$outputPageMock->method( 'getConfig' )
+			->willReturn( $config );
+
+		$outputPageMock->expects( $this->once() )
+			->method( 'addHTML' )
+			->with( '<p>WikibaseInWikitext was here</p>' );
+		$outputPageMock->expects( $this->once() )
+			->method( 'addModules' )
+			->with( 'oojs-ui-core' );
+
+		$skinMock = $this->getMockBuilder( \Skin::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		Hooks::onBeforePageDisplay( $outputPageMock, $skinMock );
+	}
+
+	/**
+	 * @covers ::onBeforePageDisplay
+	 */
+	public function testOnBeforePageDisplayVandalizeFalse() {
+		$config = new \HashConfig( [
+			'WikibaseInWikitextVandalizeEachPage' => false
+		] );
+		$outputPageMock = $this->getMockBuilder( \OutputPage::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$outputPageMock->method( 'getConfig' )
+			->willReturn( $config );
+		$outputPageMock->expects( $this->never() )
+			->method( 'addHTML' );
+		$outputPageMock->expects( $this->never() )
+			->method( 'addModules' );
+		$skinMock = $this->getMockBuilder( \Skin::class )
+			->disableOriginalConstructor()
+			->getMock();
+		Hooks::onBeforePageDisplay( $outputPageMock, $skinMock );
+	}
+
+}
